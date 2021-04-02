b544dbe54b953532687cbeae3a3d712038f3e0fa
abcemmm <1210716025@qq.com>
2021-04-02 21:16:58 +0800

    测试git

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/home/home'
-import City from '@/components/city/city'
-import Detail from '@/components/Detail/detail'
-import week from '@/components/week/week'
+// 增加注释
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/home/home";
+import City from "@/components/city/city";
+import Detail from "@/components/Detail/detail";
+import week from "@/components/week/week";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path:'/',
-      component:Home
+      path: "/",
+      component: Home,
     },
     {
-      path:'/city',
-      component:City
+      path: "/city",
+      component: City,
     },
     {
-      path:'/detail:id',
-      name:'detailLink',
-      component:Detail
+      path: "/detail:id",
+      name: "detailLink",
+      component: Detail,
     },
     {
-      path:'/week:id',
-      name:'weekLink',
-      component:week 
-    }
-  ]
-})
+      path: "/week:id",
+      name: "weekLink",
+      component: week,
+    },
+  ],
+});
